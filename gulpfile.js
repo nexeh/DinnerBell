9cbacd0610d56949155c1593d80a8df80a0eb7bc
Jeremy Corson <jeremy.r.corson@gmail.com>
2015-12-23 19:42:36 -0500

    ignoring css files now and sass now finds all sass files

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var sass = require('gulp-sass');
 var concat = require('gulp-concat');
  
 gulp.task('sass', function () {
-  gulp.src('./app/assets/dinnerbell.scss')
+  gulp.src('./app/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(concat('app.css')) // this is what was missing
     .pipe(gulp.dest('./app/assets/'));
