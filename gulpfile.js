66250b7b62b395da5ab3b584da52ace21d2e14a6
Jeremy Corson <jeremy.r.corson@gmail.com>
2015-12-23 19:32:41 -0500

    adding a gulp build process that handles sass

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+ 
+gulp.task('sass', function () {
+  gulp.src('./app/assets/dinnerbell.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concat('app.css')) // this is what was missing
+    .pipe(gulp.dest('./app/assets/'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./app/**/*.scss', ['sass']);
+});
